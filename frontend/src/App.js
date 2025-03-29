4bc7bc41a33334736056ac61991980bb38cb3d10
Aaron Jackson <aaronjackson@mines.edu>
2025-03-29 01:00:19 -0600

    cooking with multiple webpages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+
+// Create two simple components
+function Home() {
+  return <h1>Home Page</h1>;
+}
+
+function FormPage() {
+  return <h1>Form Page</h1>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world!</h1>
-      {/* You can add other components here */}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/form" element={<FormPage />} />
+      </Routes>
+    </Router>
   );
 }
 
